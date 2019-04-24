7ae05d7e8052bce79ea779a6f70881aa04682611
Robot Solution <manage.ars1@gmail.com>
2019-04-24 10:17:08 -0400

    Updated LEX Questions

--- Scripts/lex.js
+++ Scripts/lex.js
@@ -29,7 +29,7 @@ var flag = false;
             AWS.config.region = 'us-east-1';
 
             config = {
-                lexConfig: {botName: 'OttawaTwo'}
+                lexConfig: {botName: 'ottawa_topQ'}
                 //lexConfig: { botName: document.getElementById('BOT').value }
             };
 
@@ -46,53 +46,39 @@ var flag = false;
             }, function (data) {
                 flag = false;
                 console.log('Transcript: ', data.inputTranscript, ", Response: ", data.message);
-                dataKey = data.message;
+              /*  dataKey = data.message;
                 console.log("sending Key:" + dataKey);
 
                 $.ajax({
                     type: "GET",
+                    type: "GET",
                     data: {key: dataKey},
                     url: "/api/sendresponse"
-                });
+                });*/
 
                 key = data.message;
 
-                if (key === "maps") {
-                    location.href = "http://localhost:3000/Contents/Maps/index.html";
+                if (key === "Please go to the Treasurer's Office. Here's a map that shows how to get there.") {
+                    ShowPopupMap("images/maps/O-County Treasurer.png");
                 }
-                else if (key === "The Parks and Recreation Office") {
-                    ShowPopupMap("images/maps/O-Parks & Recreation.png");
+                else if (key === "Please go to the Sheriff's Office. It's to your left.") {
+                    ShowPopupMap("images/maps/O-Sheriff.png");
                 }
-                else if (key === "Fiscal Sevices") {
-                    ShowPopupMap("images/maps/O-Fiscal Services Updated .png")
+                else if (key === "Please go to the Clerks's Office. Here's a map that shows how to get there.") {
+                    ShowPopupMap("images/maps/o-Register of Deeds.png");
                 }
-                else if (key === "Sheriff's Office") {
-                    ShowPopupMap("images/maps/O-Sheriff.png")
+                else if (key === "Please go to Fiscal Services. Here's a map that shows you how to get there.") {
+                    ShowPopupMap("images/maps/O-Fiscal Services Updated .png");
                 }
-                else if (key === "Treasurer's Office") {
-                    ShowPopupMap("images/maps/O-County Treasurer.png")
+                else if (key === "Please go to Human Resrouces. It's to your right. Here's a map that shows you how to get there.") {
+                    ShowPopupMap("images/maps/O-Human Resources.png");
                 }
-                else if (key === "GIS") {
-                    ShowPopupMap("images/maps/O-GIS.png")
+                else if (key === "I'm sorry, I don't know that answer yet. Please scroll through this screen for your answer.") {
+                    window.location = "http://localhost:3000/Contents/Info/index.html";
                 }
-                else if (key === "Clerk/Register of Deeds office") {
-                    ShowPopupMap("images/maps/o-Register of Deeds.png")
-                }
-                else if (key === "Visit the Clerk/Register of Deeds office") {
-                    ShowPopupMap("images/maps/o-Register of Deeds.png")
-                }
-                else if (key === "Human Resources" || key === "Visit miottawa.org/Departments/HR/ or stop by Human Resources" || key === "Stop by Human Resources to check in" || key === "Visit the Employee Portal or Human Resources" || key === "Visit Human Resources") {
-                    ShowPopupMap("images/maps/O-Human Resources.png")
-                }
-                else if (key === "Locations vary so check with the MSU Extension Office." || key === "Yes. Please go to MSU Extension." || key === "Yes, visit MSU Extension" || key === "Yes at the MSU Extension Office or through the MSU bookstore at bookstore.msue.msu.edu") {
-                    ShowPopupMap("images/maps/O-MSU Extension.png")
-                }
-                else if (key === "Water Resources Commissioner's Office") {
-                    ShowPopupMap("images/maps/O-Water Resources Commissioner.png")
-                }
-                else if (key === "Water Resources Commissioner's Office or miottawa.org/Departments/Drain") {
-                    ShowPopupMap("images/maps/O-Water Resources Commissioner.png")
-                }
+
+
+
 
 
             }, function (error) {
