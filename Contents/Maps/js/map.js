66cb613415d3260e786e2e7bb3d8141d84e6da4d
tamoorkayani <tamoorkayani@gmail.com>
2019-05-01 22:25:13 +0500

    Fixed redirect

--- Contents/Maps/js/map.js
+++ Contents/Maps/js/map.js
@@ -3,7 +3,7 @@
 //-----------------------------------------------------
 
 function redirect(){
-    //window.location.href = "../../maincontents.htm";
+    window.location.href = "../../maincontents.htm";
 }
 var initial=setTimeout(redirect,60000);
 
